f077122deb57b2ddc238fba72bc9c49ff857aaad
Francisco Sebilla <franciscosebilla26@gmail.com>
2022-04-22 16:11:13 -0500

    added code
    nothing much, just some variables

--- Wordle+/ViewController.swift
+++ Wordle+/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var listOfWords = ["IRATE", "PIOUS", "CLOGS", "CLOTH", "OXIDE", "VOICE", "CADEN", "ISAAC", "LOREN", "FRANK", "COlIN", "ELIJA"]
+    let rowOfWords = 6
+    var currentAttemptRow = 0
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
